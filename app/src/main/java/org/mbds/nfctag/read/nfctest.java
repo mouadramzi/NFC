712212292c2b57e5d0c37db2906f61771bc56f08
Mouad ramzi <mouadrms8@gmail.com>
2023-01-18 17:20:16 +0100

    Initial Commit

---
+++ app/src/main/java/org/mbds/nfctag/read/nfctest.java
@@ -0,0 +1,14 @@
+package org.mbds.nfctag.read;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class nfctest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nfctest);
+    }
+}
